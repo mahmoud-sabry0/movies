1378d999a8f8a301df22280b24f494812569fc52
mahmoud-sabry0 <mahmoud.sabry.ahmed000@gmail.com>
2024-12-10 20:40:59 +0200

    movies

--- src/components/Movieslist/Movieslist.js
+++ src/components/Movieslist/Movieslist.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { Row } from "react-bootstrap";
-import Cardmovie from "../Cardmovie/Cardmovie";
-import PaginationComponent from "../Pagination/PaginationComponent";
+import React from 'react'
+import { Row } from 'react-bootstrap'
+import Cardmovie from '../Cardmovie/Cardmovie'
+import PaginationComponent from '../Pagination/PaginationComponent'
 
 export default function Movieslist({ movies, getPage, pageCount }) {
   return (
     <Row className="mt-3">
       {movies.length >= 1 ? (
         movies.map((mov) => {
-          return <Cardmovie key={mov.id} mov={mov} />;
+          return <Cardmovie key={mov.id} mov={mov} />
         })
       ) : (
         <h2 className="text-center">لا توجد افلام..</h2>
@@ -18,5 +18,5 @@ export default function Movieslist({ movies, getPage, pageCount }) {
         <PaginationComponent getPage={getPage} pageCount={pageCount} />
       ) : null}
     </Row>
-  );
+  )
 }
